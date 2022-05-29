c337694cc0db32b0439089248e425deac9d64eb8
maxrosca <roscamaxim2002@gmail.com>
2022-05-29 13:05:58 +0200

    refactored the dao pattern to reduce code repetition

---
+++ src/main/java/com/example/twitterclone/dao/AbstractDao.java
@@ -0,0 +1,105 @@
+package com.example.twitterclone.dao;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Interface to ease the implementation of Data Access Object principle.
+ * Unlike just accessing the database from the servlet class, for instance,
+ * this pattern provides isolation from the application layer for the persistence
+ * layer. It is used to manipulate data from any persistence mechanism. The user
+ * can get and delete the object based on its unique identifier and also save
+ * or update a certain object by providing an instance of it. There are already
+ * provided default implementations to reduce code smell.
+ *
+ * @author Rosca Maxim
+ * @param <T> the type of object that is being manipulated
+ */
+
+abstract class AbstractDao<T> {
+
+    private final EntityManager entityManager;
+    // This variable is required because using .class on type parameter is not allowed
+    // because of type erasure
+    private final Class<T> tClass;
+    // The table name of the table to be edited
+    private final String tableName;
+
+    /**
+     * Constructor for the dao class
+     *
+     * @param entityManager the spring entity manager, use autowired
+     * @param tClass the class of T, required because of type erasure
+     * @param tableName the name of the table from the database which is edited
+     */
+    AbstractDao(EntityManager entityManager, Class<T> tClass, String tableName) {
+        this.entityManager = entityManager;
+        this.tClass = tClass;
+        this.tableName = tableName;
+    }
+
+    /**
+     * Return an object from the persistence mechanism based on its unique identifier.
+     * It is recommended that the id parameter is unique.
+     *
+     * @param id the unique identifier of the object to be retrieved
+     * @return the object retrieved from the storage place
+     */
+    @Transactional
+    public T get(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<T> query = currentSession.createQuery("FROM " + tableName + " WHERE id = " + id, tClass);
+        return query.getSingleResult();
+    }
+
+    /**
+     * Returns all the objects from the persistence mechanism of type T
+     *
+     * @return a list of all the objects from the persistence mechanism of type T
+     */
+    @Transactional
+    public List<T> getAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<T> query = currentSession.createQuery("FROM " + tableName, tClass);
+        List<T> users = query.getResultList();
+        return users;
+    }
+
+    /**
+     * Takes an instance of the object and stores it to the persistence mechanism
+     *
+     * @param t the object that needs to be saved
+     */
+    @Transactional
+    public void save(T t) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.save(t);
+    }
+
+    /**
+     * Update an object from the persistence mechanism. It is recommended that the
+     * unique identifier in the instance object is not null and corresponds to the
+     * one stored. Otherwise, it is it will be hard to understand which object needs
+     * to be updated.
+     *
+     * @param t the object with the updated information and the unique identifier present
+     */
+    @Transactional
+    public void update(T t) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(t);
+    }
+    /**
+     * Delete an object from the persistence mechanism based on it's unique identifier.
+     *
+     * @param id the unique identifier of the object to be deleted
+     */
+    @Transactional
+    public void delete(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.delete(get(id));
+    }
+}
--- src/main/java/com/example/twitterclone/dao/UserDao.java
+++ src/main/java/com/example/twitterclone/dao/UserDao.java
@@ -21,84 +21,12 @@ import java.util.List;
  */
 
 @Repository
-public class UserDao implements IDao<User>{
-
-    private final EntityManager entityManager;
+public class UserDao extends AbstractDao<User>{
 
     // Using the Spring Boot EntityManager here
     @Autowired
     public UserDao(EntityManager entityManager) {
-        this.entityManager = entityManager;
+        super(entityManager, User.class, "user");
     }
 
-    /**
-     * Function that returns a User object with all the information from
-     * the database
-     *
-     * @param id the unique identifier of the object to be retrieved
-     * @return the user object from the database or null
-     */
-    @Override
-    @Transactional
-    public User get(int id) {
-        Session currentSession = entityManager.unwrap(Session.class);
-        Query<User> query = currentSession.createQuery("FROM user WHERE id = " + id, User.class);
-        return query.getSingleResult();
-    }
-
-    /**
-     * Function that returns a list of all the users from the database, if there are no
-     * users returns an empty list
-     *
-     * @return return all the users from the database or empty list
-     */
-    @Override
-    @Transactional
-    public List<User> getAll() {
-        Session currentSession = entityManager.unwrap(Session.class);
-        Query<User> query = currentSession.createQuery("FROM user", User.class);
-        List<User> users = query.getResultList();
-        return users;
-    }
-
-    /**
-     * Saves all the information of a user to the database, the id is not taken into
-     * consideration, since it is set automatically. Check the database for values
-     * that can be null to make sure there are no errors.
-     *
-     * @param user the user with all the information that needs to be added
-     */
-    @Override
-    @Transactional
-    public void save(@RequestBody User user) {
-        Session currentSession = entityManager.unwrap(Session.class);
-        currentSession.save(user);
-    }
-
-    /**
-     * Update the information of a user from the database. Can't update one's id. Make sure
-     * that the id from the provided object is a correct one, else the wrong object might
-     * be edited.
-     *
-     * @param user the object with the updated information and the id
-     */
-    @Override
-    @Transactional
-    public void update(User user) {
-        Session currentSession = entityManager.unwrap(Session.class);
-        currentSession.saveOrUpdate(user);
-    }
-
-    /**
-     * Delete an object from the database. This operation is irreversible, make sure you delete
-     * the right object.
-     *
-     * @param id the unique identifier of the object to be deleted
-     */
-    @Override
-    @Transactional
-    public void delete(int id) {
-        Session currentSession = entityManager.unwrap(Session.class);
-        currentSession.delete(get(id));
-    }
 }
